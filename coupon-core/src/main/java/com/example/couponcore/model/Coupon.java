0a79d51993b24b6aeeb112a0fd0bfea477ee198c
Bae Ji Won <qowhxk@naver.com>
2024-06-12 13:54:34 +0900

    #6- Coupon Entity Update

--- coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
+++ coupon-core/src/main/java/com/example/couponcore/model/Coupon.java
@@ -1,5 +1,6 @@
 package com.example.couponcore.model;
 
+import com.example.couponcore.exception.CouponissueException;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+
+import static com.example.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_DATE;
+import static com.example.couponcore.exception.ErrorCode.INVALID_COUPON_ISSUE_QUANTITY;
 
 @Entity
 @NoArgsConstructor
@@ -27,7 +31,7 @@ public class Coupon extends BaseTimeEntity{
     @Enumerated(value = EnumType.STRING)
     private CouponType couponType;
 
-    private Integer totlaQuantity;  // 전체 개수
+    private Integer totalQuantity;  // 전체 개수
 
     @Column(nullable = false)
     private int issuedQuantity;     // 현재 발급된 개수
@@ -43,4 +47,30 @@ public class Coupon extends BaseTimeEntity{
 
     @Column(nullable = false)
     private LocalDateTime dateIssueEnd;
+
+    // 쿠폰 개수 확인
+    public boolean availableIssueQuantity(){
+        if(totalQuantity == null){
+            return true;
+        }
+
+        return totalQuantity > issuedQuantity;
+    }
+
+    // 쿠폰 발급 기한 확인
+    public boolean availableIssueDate(){
+        LocalDateTime now = LocalDateTime.now();
+        return dateIssueStart.isBefore(now) && dateIssueEnd.isAfter(now);
+    }
+    
+    // 이슈 발생시 쿠폰 발급 개수 증가
+    public void issue(){
+        if(!availableIssueQuantity()){
+            throw new CouponissueException(INVALID_COUPON_ISSUE_QUANTITY, "Not enough coupons. total : %s, issued : %s".formatted(totalQuantity,issuedQuantity));
+        }
+        if(!availableIssueDate()){
+            throw new CouponissueException(INVALID_COUPON_ISSUE_DATE, "Date not available. request : %s, issueStart : %s, issueEnd : %s".formatted(LocalDateTime.now(),dateIssueStart,dateIssueEnd));
+        }
+        issuedQuantity++;
+    }
 }
