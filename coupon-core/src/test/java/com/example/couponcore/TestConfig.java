d29828d5f838162dbcb7ffecded160ca5f421f62
Bae Ji Won <qowhxk@naver.com>
2024-06-12 13:54:21 +0900

    #6- TestCode Add

---
+++ coupon-core/src/test/java/com/example/couponcore/TestConfig.java
@@ -0,0 +1,14 @@
+package com.example.couponcore;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+// application-core.yml에서 test를 위한 별도 설정 파일을 사용하기 위한 클래스
+@Transactional
+@ActiveProfiles("test")
+@TestPropertySource(properties = "spring.config.name=application-core")
+@SpringBootTest(classes = CouponCoreConfiguration.class)
+public class TestConfig {
+}
